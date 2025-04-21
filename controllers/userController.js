4cfab5963271ae1bbbcf544164ca89988eeedff8
Phiri-H-Roy <107152452+Phiri-Roy@users.noreply.github.com>
2025-04-21 08:51:46 +0530

    Add controllers/userController.js 1745205705652

---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+// Generate JWT token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+// User signup
+exports.signup = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // Check if user exists
+        const userExists = await User.findOne({ $or: [{ email }, { username }] });
+        if (userExists) {
+            return res.status(400).json({
+                success: false,
+                message: 'User with this email or username already exists'
+            });
+        }
+
+        // Create user
+        const user = await User.create({ username, email, password });
+
+        // Generate token
+        const token = generateToken(user._id);
+
+        res.status(201).json({
+            success: true,
+            message: 'User registered successfully',
+            token
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Server error',
+            error: error.message
+        });
+    }
+};
